c131690b827fa8ac625faa91cce3832be7dbdd74
kaique oliveira santos <kaiquevicio@gmail.com>
2020-08-22 18:28:48 -0300

    Estrutura de navegação, pronta

---
+++ modulo06/src/routes.js
@@ -0,0 +1,27 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+// Sempre colocar qual tipo de navegação dentro container
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#ccc',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
+);
+
+export default Routes;
