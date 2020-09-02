2671df81b398ada6509b244f0a0994170305522c
kaique oliveira santos <kaiquevicio@gmail.com>
2020-09-02 19:09:12 -0300

    Estilização das páginas finalizada

--- modulo07/src/pages/Cart/index.js
+++ modulo07/src/pages/Cart/index.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import {
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
-function Cart() {
-  return <div />;
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th>-</th>
+            <th>Produto</th>
+            <th>Qtd</th>
+            <th>Subtotal</th>
+            <th>-</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-tiempo-legend-8-academy-ic/18/HZM-1698-118/HZM-1698-118_zoom2.jpg?ts=1564583400&ims=326x"
+                alt="Chuteira"
+              />
+            </td>
+            <td>
+              <strong>Tênis bacana!</strong>
+              <span>R$ 125,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 250,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$ 154,99</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
-
-export default Cart;
--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -1,9 +1,110 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-tiempo-legend-8-academy-ic/18/HZM-1698-118/HZM-1698-118_zoom2.jpg?ts=1564583400&ims=326x"
+          alt="Chuteira"
+        />
+        <strong>Tênis show de bola!</strong>
+        <span>R$ 125,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 5
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-tiempo-legend-8-academy-ic/18/HZM-1698-118/HZM-1698-118_zoom2.jpg?ts=1564583400&ims=326x"
+          alt="Chuteira"
+        />
+        <strong>Tênis show de bola!</strong>
+        <span>R$ 125,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 5
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-tiempo-legend-8-academy-ic/18/HZM-1698-118/HZM-1698-118_zoom2.jpg?ts=1564583400&ims=326x"
+          alt="Chuteira"
+        />
+        <strong>Tênis show de bola!</strong>
+        <span>R$ 125,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 5
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-tiempo-legend-8-academy-ic/18/HZM-1698-118/HZM-1698-118_zoom2.jpg?ts=1564583400&ims=326x"
+          alt="Chuteira"
+        />
+        <strong>Tênis show de bola!</strong>
+        <span>R$ 125,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 5
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-tiempo-legend-8-academy-ic/18/HZM-1698-118/HZM-1698-118_zoom2.jpg?ts=1564583400&ims=326x"
+          alt="Chuteira"
+        />
+        <strong>Tênis show de bola!</strong>
+        <span>R$ 125,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 5
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-tiempo-legend-8-academy-ic/18/HZM-1698-118/HZM-1698-118_zoom2.jpg?ts=1564583400&ims=326x"
+          alt="Chuteira"
+        />
+        <strong>Tênis show de bola!</strong>
+        <span>R$ 125,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 5
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
 
 export default Home;
