982fccf1ebbbec504e1963a48f6f1f93ee88ff96
kaique oliveira santos <kaiquevicio@gmail.com>
2020-09-30 21:33:50 -0300

    Iniciando entendimento do useEffect

--- modulo08/src/App.js
+++ modulo08/src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello</h1>;
+  const [tech, setTech] = useState(['Node', 'Php']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map((techs) => (
+          <li key={techs}>{techs}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={(e) => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Add Tech
+      </button>
+    </>
+  );
 }
 
 export default App;
