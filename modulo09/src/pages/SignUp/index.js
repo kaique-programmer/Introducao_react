60058cca6aa6361747b2a4773100b3406374e9d8
kaique oliveira santos <kaiquevicio@gmail.com>
2020-10-23 00:13:03 -0300

    Estilização da tela de login, finalizada.

--- modulo09/src/pages/SignUp/index.js
+++ modulo09/src/pages/SignUp/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
   return (
-    <Container>
-      <h1>SignUp</h1>
-    </Container>
+    <>
+      <img ssrc={logo} alt="Gobarber" />
+
+      <form>
+        <input placeholder="Digite seu nome completo" />
+        <input type="email" placeholder="Digite seu e-mail" />
+        <input type="password" placeholder="Digite sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho conta</Link>
+      </form>
+    </>
   );
 }
