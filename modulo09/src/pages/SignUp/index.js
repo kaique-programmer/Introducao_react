4bce31225a1b916a1302bbe018eba09891fdaba0
kaique oliveira santos <kaiquevicio@gmail.com>
2020-10-25 23:44:50 -0300

    Validação do formulário do login, finalizado.

--- modulo09/src/pages/SignUp/index.js
+++ modulo09/src/pages/SignUp/index.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha deve ter no mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img ssrc={logo} alt="Gobarber" />
 
-      <form>
-        <input placeholder="Digite seu nome completo" />
-        <input type="email" placeholder="Digite seu e-mail" />
-        <input type="password" placeholder="Digite sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input placeholder="Digite seu nome completo" />
+        <Input type="email" placeholder="Digite seu e-mail" />
+        <Input type="password" placeholder="Digite sua senha" />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
