82cfc6771e547a1ced8dd4fa8cd9114eaceadec2
kaique oliveira santos <kaiquevicio@gmail.com>
2020-10-27 23:56:02 -0300

    Configurando autenticação do usuário

--- modulo09/src/store/modules/auth/actions.js
+++ modulo09/src/store/modules/auth/actions.js
@@ -1,3 +1,19 @@
-import { all } from 'redux-saga';
+export function signInRequest(email, password) {
+  return {
+    type: '@auth/SIGN_IN_REQUEST',
+    payload: { email, password },
+  };
+}
 
-export default all([]);
+export function signInSuccess(token, user) {
+  return {
+    type: '@auth/SIGN_IN_SUCCESS',
+    payload: { token, user },
+  };
+}
+
+export function signFailure() {
+  return {
+    type: '@auth/SIGN_FAILURE',
+  };
+}
