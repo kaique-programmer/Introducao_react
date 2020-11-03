b24310faab9742015f7b5d6c15c51666421a14e8
kaique oliveira santos <kaiquevicio@gmail.com>
2020-11-02 21:50:14 -0300

    Estilizando as notificações do usuário

---
+++ modulo09/src/components/Notifications/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { MdNotifications } from 'react-icons/md';
+
+import { Container, Badge, NotificationsList, Notification } from './styles';
+
+function Notifications() {
+  return (
+    <Container>
+      <Badge hasUnread>
+        <MdNotifications color="#7159c1" size={20} />
+      </Badge>
+      <NotificationsList>
+        <Notification unread>
+          <p>Você possui um novo agendamento para amanhã</p>
+          <time>há 2 dias</time>
+          <button type="button">Marcar como lida</button>
+        </Notification>
+        <Notification>
+          <p>Você possui um novo agendamento para amanhã</p>
+          <time>há 2 dias</time>
+          <button type="button">Marcar como lida</button>
+        </Notification>
+        <Notification>
+          <p>Você possui um novo agendamento para amanhã</p>
+          <time>há 2 dias</time>
+          <button type="button">Marcar como lida</button>
+        </Notification>
+      </NotificationsList>
+    </Container>
+  );
+}
+
+export default Notifications;
