aa163f0a95d39f97ef1239534bacab45a822c939
kaique oliveira santos <kaiquevicio@gmail.com>
2020-11-02 22:27:27 -0300

    Estilização da dashboard e notificações, finalizadas.

--- modulo09/src/components/Notifications/index.js
+++ modulo09/src/components/Notifications/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 import { MdNotifications } from 'react-icons/md';
 
-import { Container, Badge, NotificationsList, Notification } from './styles';
+import {
+  Container,
+  Badge,
+  NotificationsList,
+  Scroll,
+  Notification,
+} from './styles';
 
 function Notifications() {
   return (
@@ -11,21 +17,23 @@ function Notifications() {
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
       <NotificationsList>
-        <Notification unread>
-          <p>Você possui um novo agendamento para amanhã</p>
-          <time>há 2 dias</time>
-          <button type="button">Marcar como lida</button>
-        </Notification>
-        <Notification>
-          <p>Você possui um novo agendamento para amanhã</p>
-          <time>há 2 dias</time>
-          <button type="button">Marcar como lida</button>
-        </Notification>
-        <Notification>
-          <p>Você possui um novo agendamento para amanhã</p>
-          <time>há 2 dias</time>
-          <button type="button">Marcar como lida</button>
-        </Notification>
+        <Scroll>
+          <Notification unread>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhã</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+        </Scroll>
       </NotificationsList>
     </Container>
   );
