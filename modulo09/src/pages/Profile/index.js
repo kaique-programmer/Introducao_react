5dde6c840140aaaf224d3ae724abb459f5dc4272
kaique oliveira santos <kaiquevicio@gmail.com>
2020-11-05 23:59:17 -0300

    Estruturando página de perfil

--- modulo09/src/pages/Profile/index.js
+++ modulo09/src/pages/Profile/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 
 export default function Profile() {
   return (
     <Container>
-      <h1>Profile</h1>
+      <Form>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Ok</button>
+      </Form>
     </Container>
   );
 }
