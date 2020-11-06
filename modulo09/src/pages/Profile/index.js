40f0714f743100386b43f164b37b7f5145a14d13
kaique oliveira santos <kaiquevicio@gmail.com>
2020-11-06 00:20:05 -0300

    Página de perfil, estilizada.

--- modulo09/src/pages/Profile/index.js
+++ modulo09/src/pages/Profile/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
     <Container>
-      <Form>
-        <Input name="email" type="email" placeholder="Seu e-mail" />
-        <Input name="password" type="password" placeholder="Sua senha" />
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu E-mail" />
 
-        <button type="submit">Ok</button>
+        <hr />
+
+        <Input
+          name="password"
+          type="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
       </Form>
+
+      <button type="button">Sair do Gobarber</button>
     </Container>
   );
 }
