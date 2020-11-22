a0eefc43c43fdf4c6a782ccf4f7e6040c6ebb0cb
kaique oliveira santos <kaiquevicio@gmail.com>
2020-11-22 14:59:34 -0300

    Criação das estruturas de components da aplicação mobile

--- modulo09/mobile/src/pages/SignIn/index.js
+++ modulo09/mobile/src/pages/SignIn/index.js
@@ -1,10 +1,27 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
+
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 const SignIn = () => {
-  return <View />;
+  return (
+    <Background>
+      <Text>Signin</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>OK</Button>
+    </Background>
+  );
 };
 
 export default SignIn;
