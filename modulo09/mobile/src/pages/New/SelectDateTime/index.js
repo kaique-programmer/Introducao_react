67e87e66a1909858f2e9938e7b039298fc179385
kaique oliveira santos <kaiquevicio@gmail.com>
2020-12-04 18:07:06 -0300

    Iniciando estrutura para configurar as entradas de data do usuário

--- modulo09/mobile/src/pages/New/SelectDateTime/index.js
+++ modulo09/mobile/src/pages/New/SelectDateTime/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <DateInput />
+      </Container>
+    </Background>
+  );
 }
 
-SelectDateTime.navigationOptions = {
+SelectDateTime.navigationOptions = ({ navigation }) => ({
   title: 'Selecione a data e horário',
-};
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
