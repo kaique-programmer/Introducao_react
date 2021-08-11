20a3cb0fd48ad430f24ab3e2de18bcf5bd2d25c9
kaique oliveira santos <kaiquevicio@gmail.com>
2021-08-11 19:16:16 -0300

    initial studies

--- course_React_Next/project01/src/App.js
+++ course_React_Next/project01/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'; 
 
-function App() {
+class App extends Component {
+
+  state = {
+    name: 'kaique',
+    counter: 0
+  }
+
+  handlePClik = () => {
+    this.setState({ name: 'oliver' });
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
+  render() {
+    const {name, counter} = this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="App-header">        
+        <p onClick={this.handlePClik}>
+          {name}{counter}
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={this.handleAClick}
         >
-          Learn React
+          link contador
         </a>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
