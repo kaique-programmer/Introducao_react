580d360994a38b04f65bc7e7b007caa4c98ea714
kaique oliveira santos <kaiquevicio@gmail.com>
2021-09-08 18:44:22 -0300

    Add mock service work

--- course_React_Next/project01/src/templates/Home/Home.test.jsx
+++ course_React_Next/project01/src/templates/Home/Home.test.jsx
@@ -1,5 +1,48 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { Home } from '.';
+
+const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
+    return res(ctx.json([
+      {
+        userId: 1,
+        id: 1,
+        title: 'title one',
+        body: 'body one',
+        url: 'img1.jpg'
+      },
+      {
+        userId: 2,
+        id: 2,
+        title: 'title two',
+        body: 'body two',
+        url: 'img2.jpg'
+      }
+    ]))
+  })
+]
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  it('write in english', () => {
-    expect(2).toBe(1);
+
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should render searc, posts and load more', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('not exist posts');
+
+    await waitForElementToBeRemoved(noMorePosts);
+    screen.debug();
   });
 });
