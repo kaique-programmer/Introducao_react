ccd529073df0a8ff72c0868396bc1a3f224b1a79
kaique oliveira santos <kaiquevicio@gmail.com>
2021-09-17 23:59:33 -0300

    useMemo

--- course_React_Next/project02/src/App.js
+++ course_React_Next/project02/src/App.js
@@ -1,37 +1,52 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable no-shadow */
 /* eslint-disable no-console */
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/button-has-type */
 import Props from 'prop-types';
+import { useEffect, useMemo, useState } from 'react';
 
-import React, { useCallback, useState } from 'react';
-
-const Button = React.memo(({ incrementButton }) => {
+const Post = ({ post }) => {
   console.log('filho, renderizou');
-  return <button onClick={() => incrementButton(10)}>+</button>;
-});
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>;
+};
 
-Button.propTypes = {
-  incrementButton: Props.func,
+Props.propTypes = {
+  post: Props.shape({
+    id: Props.number,
+    title: Props.string,
+    body: Props.string,
+  }),
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  const incrementCounter = useCallback((number) => {
-    setCounter((c) => c + number);
-  }, []);
-
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
   console.log('pai, renderizou');
+
+  // componentDidMount
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((response) => setPosts(response));
+    }, 5000);
+  }, []);
 
   return (
     <div className="App">
-      <p>Teste</p>
-      <h1>
-        c1:
-        {counter}
-      </h1>
-      <Button incrementButton={incrementCounter} />
+      <p>
+        <input type="search" value={value} onChange={(event) => setValue(event.target.value)} />
+      </p>
+      {useMemo(() => (
+        posts.length > 0
+          && posts.map((post) => <Post key={post.id} post={post} />)
+      ), [posts])}
+
+      {posts.length <= 0 && (<p>Wait! not exist posts</p>)}
     </div>
   );
 }
