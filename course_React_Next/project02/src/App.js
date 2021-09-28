438d8776ebb23841d74d216604a55b268f650284
kaique oliveira santos <kaiquevicio@gmail.com>
2021-09-27 22:28:32 -0300

    useReducer part 1

--- course_React_Next/project02/src/App.js
+++ course_React_Next/project02/src/App.js
@@ -7,14 +7,48 @@
 /* eslint-disable no-console */
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/button-has-type */
+import { useReducer } from 'react';
 import AppContext from './contexts/AppContext';
 import Div from './components/Div';
 
+const globalState = {
+  title: 'the title context',
+  body: 'the body context',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'change': {
+      console.log('call change', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'reverse': {
+      console.log('call reverse');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+    default: break;
+  }
+
+  console.log('nothing action finded');
+  return { ...state };
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title}
+        {' '}
+        {counter}
+      </h1>
+      <button onClick={() => dispatch({ type: 'change', payload: new Date().toLocaleDateString('pt-BR') })}>Click</button>
+      <button onClick={() => dispatch({ type: 'reverse' })}>reverse</button>
+    </div>
   );
 }
 
