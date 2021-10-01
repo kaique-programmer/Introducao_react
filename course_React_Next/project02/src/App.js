66d50f822b2569521c2ee344320622bb9233a310
kaique oliveira santos <kaiquevicio@gmail.com>
2021-10-01 19:35:03 -0300

    restructured

--- course_React_Next/project02/src/App.js
+++ course_React_Next/project02/src/App.js
@@ -9,7 +9,6 @@
 /* eslint-disable react/button-has-type */
 import { useReducer } from 'react';
 import AppContext from './contexts/AppContext';
-import Div from './components/Div';
 
 const globalState = {
   title: 'the title context',
@@ -17,37 +16,10 @@ const globalState = {
   counter: 0,
 };
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'change': {
-      console.log('call change', action.payload);
-      return { ...state, title: action.payload };
-    }
-    case 'reverse': {
-      console.log('call reverse');
-      const { title } = state;
-      return { ...state, title: title.split('').reverse().join('') };
-    }
-    default: break;
-  }
-
-  console.log('nothing action finded');
-  return { ...state };
-};
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, globalState);
-  const { counter, title, body } = state;
-
   return (
     <div>
-      <h1>
-        {title}
-        {' '}
-        {counter}
-      </h1>
-      <button onClick={() => dispatch({ type: 'change', payload: new Date().toLocaleDateString('pt-BR') })}>Click</button>
-      <button onClick={() => dispatch({ type: 'reverse' })}>reverse</button>
+      <h1>Hello</h1>
     </div>
   );
 }
