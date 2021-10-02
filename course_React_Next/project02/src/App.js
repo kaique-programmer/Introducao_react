c11ec31f47718e95c4af219b76e049d15689810a
kaique oliveira santos <kaiquevicio@gmail.com>
2021-10-01 22:52:19 -0300

    my hook

--- course_React_Next/project02/src/App.js
+++ course_React_Next/project02/src/App.js
@@ -5,78 +5,53 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable react/require-default-props */
 /* eslint-disable no-undef */
-import Props from 'prop-types';
-import {
-  createContext, useContext, useReducer, useRef,
-} from 'react';
 
-// actions.js
-export const actions = {
-  CHANGE_TITLE: 'CHANGE_TITLE',
-};
+import { useEffect, useRef, useState } from 'react';
 
-// data.js
-export const globalState = {
-  title: 'the title context',
-  body: 'the body context',
-  counter: 0,
-};
+const useMyHook = (callback, delay) => {
+  const saveCallback = useRef();
 
-// reducer.js
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case actions.CHANGE_TITLE: {
-      console.log('Mudar título');
-      return { ...state, title: action.payload };
-    }
-  }
+  useEffect(() => {
+    saveCallback.current = callback;
+  }, [callback]);
 
-  return { ...state };
-};
+  useEffect(() => {
+    const interval = setInterval(() => {
+      saveCallback.current();
+    }, delay);
 
-// AppContext.jsx
-export const Context = createContext();
-export const AppContext = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, globalState);
-
-  const changeTitle = (payload) => {
-    dispatch({ type: actions.CHANGE_TITLE, payload });
-  };
-
-  return (
-    <Context.Provider value={{ state, changeTitle }}>
-      {children}
-    </Context.Provider>
-  );
-};
-
-AppContext.propTypes = {
-  children: Props.node,
-};
-
-// H1/index.jsx
-export const H1 = () => {
-  const context = useContext(Context);
-  const inputRef = useRef();
-
-  return (
-    <>
-      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>
-        {context.state.title}
-      </h1>
-      <input type="text" ref={inputRef} />
-    </>
-  );
+    return () => clearInterval(interval);
+  }, [delay]);
 };
 
 // App.jsx
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [delay, setDelay] = useState(1000);
+  const [increment, setIncrement] = useState(100);
+
+  useMyHook(() => setCounter((c) => c + 1), delay);
+
   return (
-    <AppContext>
-      <div>
-        <H1 />
-      </div>
-    </AppContext>
+    <div>
+      <h1>
+        Contador
+        {counter}
+      </h1>
+      <h1>
+        Delay
+        {delay}
+      </h1>
+      <button type="button" onClick={() => setDelay((d) => d + increment)}>
+        +
+        {increment}
+      </button>
+      <button type="button" onClick={() => setDelay((d) => d - increment)}>
+        -
+        {increment}
+      </button>
+      <input type="number" value={increment} onChange={(e) => { setIncrement(Number(e.target.value)); }} />
+    </div>
   );
 }
 
