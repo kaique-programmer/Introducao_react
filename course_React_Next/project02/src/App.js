ba1c4c96217969bc36ff88ebf5e924f93956f62e
kaique oliveira santos <kaiquevicio@gmail.com>
2021-10-02 23:30:15 -0300

    restructured

--- course_React_Next/project02/src/App.js
+++ course_React_Next/project02/src/App.js
@@ -6,51 +6,11 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable no-undef */
 
-import { useEffect, useRef, useState } from 'react';
-
-const useMyHook = (callback, delay) => {
-  const saveCallback = useRef();
-
-  useEffect(() => {
-    saveCallback.current = callback;
-  }, [callback]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      saveCallback.current();
-    }, delay);
-
-    return () => clearInterval(interval);
-  }, [delay]);
-};
-
 // App.jsx
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [delay, setDelay] = useState(1000);
-  const [increment, setIncrement] = useState(100);
-
-  useMyHook(() => setCounter((c) => c + 1), delay);
-
   return (
     <div>
-      <h1>
-        Contador
-        {counter}
-      </h1>
-      <h1>
-        Delay
-        {delay}
-      </h1>
-      <button type="button" onClick={() => setDelay((d) => d + increment)}>
-        +
-        {increment}
-      </button>
-      <button type="button" onClick={() => setDelay((d) => d - increment)}>
-        -
-        {increment}
-      </button>
-      <input type="number" value={increment} onChange={(e) => { setIncrement(Number(e.target.value)); }} />
+      <h1>hello</h1>
     </div>
   );
 }
