4511d5bbac438012ecce2c08577868f1fd880c21
kaique oliveira santos <kaiquevicio@gmail.com>
2021-10-06 18:43:26 -0300

    useImperativeHandle and React.forwardRef

--- course_React_Next/project02/src/components/Home/index.jsx
+++ course_React_Next/project02/src/components/Home/index.jsx
@@ -1,4 +1,9 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/prop-types */
+import {
+  forwardRef, useImperativeHandle, useLayoutEffect, useRef, useState,
+} from 'react';
 
 const Home = () => {
   const [counted, setCounted] = useState([0, 1, 2, 3, 4, 5]);
@@ -7,11 +12,13 @@ const Home = () => {
 
   useLayoutEffect(() => {
     const now = Date.now();
-    while (Date.now < now + 3000) { divRef.current.scrollTop = divRef.current.scrollHeight; }
+    while (Date.now < now + 3000);
+    divRef.current.divRef.scrollTop = divRef.current.divRef.scrollHeight;
   });
 
   function handleClick() {
     setCounted((c) => [...c, +c.slice(-1) + 1]);
+    divRef.current.handleClick();
   }
 
   return (
@@ -21,11 +28,36 @@ const Home = () => {
         {' '}
         {counted.slice(-1)}
       </button>
-      <div ref={divRef} style={{ height: '100px', width: '100px', overflowY: 'scroll' }}>
-        {counted.map((c) => <p key={`c-${c}`}>{c}</p>)}
-      </div>
+      <DisplayCounted counted={counted} ref={divRef} />
     </>
   );
 };
+
+export const DisplayCounted = forwardRef(({ counted }, ref) => {
+  const [rand, setRand] = useState('0.24');
+  const divRef = useRef();
+
+  function handleClick() {
+    setRand(Math.random().toFixed(2));
+  }
+
+  useImperativeHandle(ref, () => ({
+    handleClick,
+    divRef: divRef.current,
+  }));
+
+  return (
+    <div ref={divRef} style={{ height: '100px', width: '100px', overflowY: 'scroll' }}>
+      {counted.map((c) => (
+        <p onClick={handleClick} key={`c-${c}`}>
+          {c}
+          {' '}
+          +++
+          {rand}
+        </p>
+      ))}
+    </div>
+  );
+});
 
 export default Home;
