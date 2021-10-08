0c6149c1158820f3aa7fd7ff8975d879e4282f20
kaique oliveira santos <kaiquevicio@gmail.com>
2021-10-07 23:45:24 -0300

    Error Boundaries - errors control

--- course_React_Next/project02/src/components/Home/index.jsx
+++ course_React_Next/project02/src/components/Home/index.jsx
@@ -1,43 +1,95 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable no-shadow */
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/prop-types */
-import { useDebugValue, useEffect, useState } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import { Component, useEffect, useState } from 'react';
 
-const useMediaQuery = (queryValue, initialValue = false) => {
-  const [match, setMatch] = useState(initialValue);
+const s = {
+  style: {
+    fontSize: '60px',
+  },
+};
 
-  useDebugValue(`query: ${queryValue}`, (name) => `${name}modified`);
+class MyErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update the state so that the next rendering will show the alternative UI.
+
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service.
+    // console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any alternative UI
+      return <p {...s}> = Something went wrong(</p>;
+    }
+
+    return this.props.children;
+  }
+}
+
+const ItWillThrowError = () => {
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    let isMounted = true;
-    const matchMedia = window.matchMedia(queryValue);
+    if (counter > 3) {
+      throw new Error('That boring!!!');
+    }
+  }, [counter]);
 
-    const handleChange = () => {
-      if (!isMounted) return;
-      setMatch(Boolean(matchMedia.matches));
-    };
-
-    matchMedia.addEventListener('change', handleChange);
-    setMatch(!!matchMedia.matches);
-
-    return () => {
-      isMounted = false;
-      matchMedia.removeEventListener('change', handleChange);
-    };
-  }, [queryValue]);
-  return match;
+  return (
+    <div>
+      <button type="button" {...s} onClick={() => setCounter((s) => s + 1)}>
+        increase
+        {' '}
+        {counter}
+      </button>
+    </div>
+  );
 };
 
-const Home = () => {
-  const huge = useMediaQuery('(min-width: 980px)');
-  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)');
-  const medium = useMediaQuery('(max-width: 767px) and (min-width: 321px)');
-  const small = useMediaQuery('(max-width: 321px)');
-
-  const background = huge ? 'green' : big ? 'red' : medium ? 'yellow' : small ? 'salmon' : null;
-
-  return <div style={{ fontSize: '60px', background }}>Oi</div>;
-};
+const Home = () => (
+  <div {...s}>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+    <MyErrorBoundary>
+      <ItWillThrowError />
+    </MyErrorBoundary>
+  </div>
+);
 
 export default Home;
